54655945b4aefd65c0477cef011fd3680e7b5c8f
Luis Carlos <100967943+LuisCarlosss@users.noreply.github.com>
2022-08-28 00:07:47 -0300

    deu ruim

--- login.py
+++ login.py
@@ -1,65 +1,56 @@
-import mysql.connector 
+import mysql.connector
 
-#conxeão com banco de dados sistema_login
+# conxeão com banco de dados sistema_login
 try:
+    global conexão, cursor
     conexão = mysql.connector.connect(
-        host = 'localhost',
-        user = 'root',
-        password = '',
-        database = 'sistema_login'
+        host='localhost',
+        user='root',
+        password='',
+        database='sistema_login'
     )
+    cursor = conexão.cursor()
 except Exception as erro:
     print(f'Falha a conexão com banco de dados. ERRO [{erro.__class__}] ')
 else:
     print('Conexão com banco de dados feita com sucesso')
 
-cursor = conexão.cursor()
+
+# Sistema de login
+def cadastroUsuario(cadastro_nome, cadastro_senha):
+
+    comando_inserir = f'''INSERT INTO usuarios VALUES
+    (DEFAULT,'{cadastro_nome}','{cadastro_senha}');
+    '''
+    cursor.execute(comando_inserir)
+    conexão.commit()
+    print('Usuário cadastrado com sucesso!')
 
 
-
-
-#Sistema de login
-def cadastro_usuario():
-
-    print('Tela de cadastro')
-    cadastro_nome = str(input('Digite um username:')) 
-    cadastro_senha = str(input('Digite uma senha'))
-
-    def inserir_dados():
-        comando_inserir = f'''INSERT INTO usuarios VALUES
-        (DEFAULT,'{cadastro_nome}','{cadastro_senha}');
-        '''
-        cursor.execute(comando_inserir)
-        conexão.commit()
-        print('Usuário cadastrado com sucesso!')
-        inserir_dados()
-def exibir():
+def exibirCadastrados():
     cursor.execute('SELECT * FROM usuarios;')
-    res = cursor.fetchall()
-    for linha in res:
+    consultaBanco = cursor.fetchall()
+    if consultaBanco == []:
+        print('Banco de dados vázio!')
+    for linha in consultaBanco:
         print(linha)
 
-def login(): 
-    #logar 
-    print('Faça seu login')
-    login_nome = str(input('Nome:'))
-    login_senha = str(input('Senha:'))
 
-    def verificação():
-            comando_coletar = f'''SELECT username,senha FROM usuarios
-            WHERE username = '{login_nome}';
-            '''
-            cursor.execute(comando_coletar)
-            dados = cursor.fetchall()
+def logarNoSistema(login_nome, login_senha):
 
-            if login_nome == dados[0][0]:
-                print('nome correto!')
-                if login_senha == dados[0][1]:
-                    print('Senha correta!')
-                    print(f'{login_nome} Logado com sucessor!')
-            else:
-                print('Nome ou senha incorreto!')
-    verificação()
+    comando_coletar = f'''SELECT username,senha FROM usuarios
+        WHERE username = '{login_nome}';
+        '''
+    cursor.execute(comando_coletar)
+    dados = cursor.fetchall()
+
+    if login_nome == dados[0][0]:
+        if login_senha == dados[0][1]:
+            print(f'{login_nome} Logado com sucessor!')
+        else:
+            print('Senha incorreta!')
+    else:
+        print('Nome incorreto!')
 
 
 while True:
@@ -67,18 +58,21 @@ while True:
     print('''
     [0] logar
     [1] Cadastra usuario
-    [2] Sair 
-    [3] Exibir tudo''')
-    opç = int(input('Digite um numero inteiro:'))
+    [2] Sair do sistema
+    [3] Exibir contas cadastradas\n''')
+    opç = int(input('Digite um numero inteiro > '))
 
     if opç == 0:
-        login()
+        logarNoSistema(str(input('nome > ')),
+                       str(input('senha > ')))
     elif opç == 1:
-        cadastro_usuario()
+        cadastroUsuario(str(input('Digite nome > ')),
+                        str(input('Digite senha > ')))
     elif opç == 3:
-        exibir()
+        exibirCadastrados()
     elif opç == 2:
-        print('Parando...')
+        print('Saindo...')
         break
     else:
-        print('Deu ruim')
+        print('Numero não encontrado, Tente novamente!')
+        continue
